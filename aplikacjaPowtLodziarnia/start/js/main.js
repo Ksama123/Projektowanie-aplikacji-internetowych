5bad33a45bfe24c44ad46caa9de00e4b86519140
Ksama123 <ksawiergilner@proton.me>
2026-09-02 16:28:19 +0200

    Add files via upload

--- aplikacjaPowtLodziarnia/start/js/main.js
+++ aplikacjaPowtLodziarnia/start/js/main.js
@@ -1,1 +1,14 @@
 // Kalkulator — działa na zamowienie.html (#oblicz, walidacja 1–5, #wynik)
+const wynik = document.querySelector(#wynik);
+const oblicz = document.querySelector(#oblicz);
+
+oblicz.addEventListener("click", function (e) {
+  const ilosc = document.querySelector("#ilosc").value;
+  const smak = document.querySelector("#smak").value;
+  gotowe = ilosc * smak;
+  if(gotowe>0){
+    wynik.innertext
+  }
+
+  
+});
